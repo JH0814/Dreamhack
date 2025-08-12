34c7283c8cb9ccc12be32719b57bdf00d9efa667
kimjeonghwan0814 <everydayeffort@naver.com>
2025-08-12 16:43:59 +0900

    2025/08/12

---
+++ EZ-Cube/EZ-Cube.py
@@ -0,0 +1,190 @@
+Sbox = [
+    [99, 251, 15], [124, 16, 202], [119, 245, 230], [123, 33, 253], [242, 137, 167],
+    [107, 127, 210], [111, 168, 194], [197, 166, 36], [48, 4, 242], [1, 124, 16],
+    [103, 133, 151], [43, 241, 161], [254, 187, 234], [215, 14, 171], [171, 98, 170],
+    [118, 56, 7], [202, 116, 146], [130, 19, 125], [201, 221, 193], [125, 255, 22],
+    [250, 45, 216], [89, 203, 31], [71, 160, 224], [240, 140, 100], [173, 149, 42],
+    [212, 72, 82], [162, 58, 128], [175, 121, 182], [156, 222, 29], [164, 73, 59],
+    [114, 64, 9], [192, 186, 244], [183, 169, 211], [253, 84, 32], [147, 220, 134],
+    [38, 247, 104], [54, 5, 107], [63, 117, 157], [247, 104, 69], [204, 75, 179],
+    [52, 24, 173], [165, 6, 111], [229, 217, 53], [241, 161, 50], [113, 163, 10],
+    [216, 97, 239], [49, 199, 198], [21, 89, 203], [4, 242, 137], [199, 198, 180],
+    [35, 38, 247], [195, 46, 49], [24, 173, 149], [150, 144, 96], [5, 107, 127],
+    [154, 184, 108], [7, 197, 166], [18, 201, 221], [128, 205, 189], [226, 152, 70],
+    [235, 233, 30], [39, 204, 75], [178, 55, 154], [117, 157, 94], [9, 1, 124],
+    [131, 236, 206], [44, 113, 163], [26, 162, 58], [27, 175, 121], [110, 159, 219],
+    [90, 190, 174], [160, 224, 225], [82, 0, 99], [59, 226, 152], [214, 246, 66],
+    [179, 109, 60], [41, 165, 6], [227, 17, 130], [47, 21, 89], [132, 95, 207],
+    [83, 237, 85], [209, 62, 178], [0, 99, 251], [237, 85, 252], [32, 183, 169],
+    [252, 176, 231], [177, 200, 232], [91, 57, 18], [106, 2, 119], [203, 31, 192],
+    [190, 174, 228], [57, 18, 201], [74, 214, 246], [76, 41, 165], [88, 106, 2],
+    [207, 138, 126], [208, 112, 81], [239, 223, 158], [170, 172, 145], [251, 15, 118],
+    [67, 26, 162], [77, 227, 17], [51, 195, 46], [133, 151, 136], [69, 110, 159],
+    [249, 153, 238], [2, 119, 245], [127, 210, 181], [80, 83, 237], [60, 235, 233],
+    [159, 219, 185], [168, 194, 37], [81, 209, 62], [163, 10, 103], [64, 9, 1],
+    [143, 115, 143], [146, 79, 132], [157, 94, 88], [56, 7, 197], [245, 230, 142],
+    [188, 101, 77], [182, 78, 47], [218, 87, 91], [33, 253, 84], [16, 202, 116],
+    [255, 22, 71], [243, 13, 215], [210, 181, 213], [205, 189, 122], [12, 254, 187],
+    [19, 125, 255], [236, 206, 139], [95, 207, 138], [151, 136, 196], [68, 27, 175],
+    [23, 240, 140], [196, 28, 156], [167, 92, 74], [126, 243, 13], [61, 39, 204],
+    [100, 67, 26], [93, 76, 41], [25, 212, 72], [115, 143, 115], [96, 208, 112],
+    [129, 12, 254], [79, 132, 95], [220, 134, 68], [34, 147, 220], [42, 229, 217],
+    [144, 96, 208], [136, 196, 28], [70, 90, 190], [238, 40, 52], [184, 108, 80],
+    [20, 250, 45], [222, 29, 164], [94, 88, 106], [11, 43, 241], [219, 185, 86],
+    [224, 225, 248], [50, 35, 38], [58, 128, 205], [10, 103, 133], [73, 59, 226],
+    [6, 111, 168], [36, 54, 5], [92, 74, 214], [194, 37, 63], [211, 102, 51],
+    [172, 145, 129], [98, 170, 172], [145, 129, 12], [149, 42, 229], [228, 105, 249],
+    [121, 182, 78], [231, 148, 34], [200, 232, 155], [55, 154, 184], [109, 60, 235],
+    [141, 93, 76], [213, 3, 123], [78, 47, 21], [169, 211, 102], [108, 80, 83],
+    [86, 177, 200], [244, 191, 8], [234, 135, 23], [101, 77, 227], [122, 218, 87],
+    [174, 228, 105], [8, 48, 4], [186, 244, 191], [120, 188, 101], [37, 63, 117],
+    [46, 49, 199], [28, 156, 222], [166, 36, 54], [180, 141, 93], [198, 180, 141],
+    [232, 155, 20], [221, 193, 120], [116, 146, 79], [31, 192, 186], [75, 179, 109],
+    [189, 122, 218], [139, 61, 39], [138, 126, 243], [112, 81, 209], [62, 178, 55],
+    [181, 213, 3], [102, 51, 195], [72, 82, 0], [3, 123, 33], [246, 66, 44],
+    [14, 171, 98], [97, 239, 223], [53, 150, 144], [87, 91, 57], [185, 86, 177],
+    [134, 68, 27], [193, 120, 188], [29, 164, 73], [158, 11, 43], [225, 248, 65],
+    [248, 65, 131], [152, 70, 90], [17, 130, 19], [105, 249, 153], [217, 53, 150],
+    [142, 25, 212], [148, 34, 147], [155, 20, 250], [30, 114, 64], [135, 23, 240],
+    [233, 30, 114], [206, 139, 61], [85, 252, 176], [40, 52, 24], [223, 158, 11],
+    [140, 100, 67], [161, 50, 35], [137, 167, 92], [13, 215, 14], [191, 8, 48],
+    [230, 142, 25], [66, 44, 113], [104, 69, 110], [65, 131, 236], [153, 238, 40],
+    [45, 216, 97], [15, 118, 56], [176, 231, 148], [84, 32, 183], [187, 234, 135],
+    [22, 71, 160],
+]
+
+Rcon = (
+    0x00, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40,
+    0x80, 0x1B, 0x36, 0x6C, 0xD8, 0xAB, 0x4D, 0x9A,
+    0x2F, 0x5E, 0xBC, 0x63, 0xC6, 0x97, 0x35, 0x6A,
+    0xD4, 0xB3, 0x7D, 0xFA, 0xEF, 0xC5, 0x91, 0x39,
+)
+
+
+def text2matrix(text):
+    matrix = [[0] * 4 for _ in range(4)]
+    for i in range(16):
+        matrix[i % 4][i // 4] = (text >> (8 * (15 - i))) & 0xFF
+    return matrix
+
+
+def matrix2text(matrix):
+    text = 0
+    for i in range(4):
+        for j in range(4):
+            text |= (matrix[j][i] << (120 - 8 * (4 * i + j)))
+    return text
+
+
+def sbox_lookup(x):
+    for i in range(3):
+        x = Sbox[x][i]
+    return x
+
+class AESDecryptor:
+    def __init__(self, master_key):
+        self.inv_sbox = self._create_inv_sbox()
+        self.round_keys = self._key_expansion(master_key)
+
+    def _create_inv_sbox(self):
+        inv_sbox = [0] * 256
+        for i in range(256):
+            try:
+                output = sbox_lookup(i)
+                inv_sbox[output] = i
+            except IndexError:
+                pass
+        return inv_sbox
+
+    def _key_expansion(self, master_key):
+        round_keys = text2matrix(master_key)
+        for i in range(4, 4 * 11):
+            round_keys.append([])
+            if i % 4 == 0:
+                prev = round_keys[i - 1]
+                prev_rot = [prev[1], prev[2], prev[3], prev[0]]
+                sub = [sbox_lookup(b) for b in prev_rot]
+                rcon_byte = Rcon[i // 4]
+                byte = round_keys[i - 4][0] ^ sub[0] ^ rcon_byte
+                round_keys[i].append(byte)
+                for j in range(1, 4):
+                    byte = round_keys[i - 4][j] ^ sub[j]
+                    round_keys[i].append(byte)
+            else:
+                for j in range(4):
+                    byte = round_keys[i - 4][j] ^ round_keys[i - 1][j]
+                    round_keys[i].append(byte)
+        return round_keys
+    
+    def _gmul(self, a, b):
+        p = 0
+        for _ in range(8):
+            if b & 1:
+                p ^= a
+            hi_bit_set = a & 0x80
+            a <<= 1
+            if hi_bit_set:
+                a ^= 0x1b
+            b >>= 1
+        return p & 0xFF
+
+    def __add_round_key(self, s, k):
+        for i in range(4):
+            for j in range(4):
+                s[i][j] ^= k[i][j]
+
+    def __inv_sub_bytes(self, s):
+        for i in range(4):
+            for j in range(4):
+                s[i][j] = self.inv_sbox[s[i][j]]
+
+    def __inv_shift_rows(self, s):
+        s[1][0], s[1][1], s[1][2], s[1][3] = s[1][3], s[1][0], s[1][1], s[1][2]
+        s[2][0], s[2][1], s[2][2], s[2][3] = s[2][2], s[2][3], s[2][0], s[2][1]
+        s[3][0], s[3][1], s[3][2], s[3][3] = s[3][1], s[3][2], s[3][3], s[3][0]
+
+    def __inv_mix_columns(self, s):
+        for i in range(4):
+            col = s[i]
+            c0, c1, c2, c3 = col[0], col[1], col[2], col[3]
+            col[0] = self._gmul(c0, 0x0e) ^ self._gmul(c1, 0x0b) ^ self._gmul(c2, 0x0d) ^ self._gmul(c3, 0x09)
+            col[1] = self._gmul(c0, 0x09) ^ self._gmul(c1, 0x0e) ^ self._gmul(c2, 0x0b) ^ self._gmul(c3, 0x0d)
+            col[2] = self._gmul(c0, 0x0d) ^ self._gmul(c1, 0x09) ^ self._gmul(c2, 0x0e) ^ self._gmul(c3, 0x0b)
+            col[3] = self._gmul(c0, 0x0b) ^ self._gmul(c1, 0x0d) ^ self._gmul(c2, 0x09) ^ self._gmul(c3, 0x0e)
+
+    def decrypt(self, ciphertext):
+        state = text2matrix(ciphertext)
+        self.__add_round_key(state, self.round_keys[40:])
+        self.__inv_shift_rows(state)
+        self.__inv_sub_bytes(state)
+        for i in range(9, 0, -1):
+            self.__add_round_key(state, self.round_keys[4 * i : 4 * (i + 1)])
+            self.__inv_mix_columns(state)
+            self.__inv_shift_rows(state)
+            self.__inv_sub_bytes(state)
+        self.__add_round_key(state, self.round_keys[:4])
+        return matrix2text(state)
+
+def str_to_blocks(s, blocksize=16):
+    b = s.encode('utf-8')
+    blocks = []
+    for i in range(0, len(b), blocksize):
+        chunk = b[i:i+blocksize]
+        if len(chunk) < blocksize:
+            chunk = chunk.ljust(blocksize, b'\x00')
+        blocks.append(int.from_bytes(chunk, 'big'))
+    return blocks
+
+
+def blocks_to_str(blocks, blocksize=16):
+    b = b''.join(x.to_bytes(blocksize, 'big') for x in blocks)
+    return b.rstrip(b'\x00').decode('utf-8', errors='replace')
+
+
+key = 0x48656c6c6f45766572796f6e65557755
+ciphertext = [0xa215ead9455798fc354a392996d080ca, 0xbab08bf362d5448ff41b00f70d8e8662, 0xbd519fc918aa6b251c864d08d00c2e40, 0xe3857332b9a5372745c9692d028e8f21, 0x776adc9ed1b1373010cdc42bd5f3e2d6, 0x7e978a5a64a51823434a5c821f4eeaf2, 0x11c3cfc58c7f60757444265c06a44f7e, 0x897bcd70e981933f701c603a1b472336, 0xf85f377ea85a86b463f26132f2aba546, 0x1e6cdac948096ffaa01409aaa87ad60f, 0xc65d6dea279c08800376fa94619dfe9e, 0x40a8680ccf3a8ee56ec7536858abc298, 0xcb8f0ad36a9ef3750e4de25054ebe5dc, 0x2fa1f5cd992047e6de1ad436e0f1c803, 0x774372c1cae4e8b153ca89277726aa54, 0x4b4dc48124c6c1dbd8492db1c29c97de]
+
+decryptor = AESDecryptor(key)
+pt_blocks = [decryptor.decrypt(block) for block in ciphertext]
+decrypted_str = blocks_to_str(pt_blocks)
+print("Plaintext : ", decrypted_str)
